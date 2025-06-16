a8eeee87b1725c3d77cb8186442dce14730d9f6f
Srivatsa Kuchibhotla <sk13g23@soton.ac.uk>
2025-06-16 05:16:29 +0100

    fixed flake8 erros

--- life/life.py
+++ life/life.py
@@ -2,13 +2,13 @@ import numpy as np
 from matplotlib import pyplot
 from scipy.signal import convolve2d
 
-glider = np.array([[0,1,0],[0,0,1],[1,1,1]])
+glider = np.array([[0, 1, 0], [0, 0, 1], [1, 1, 1]])
 
 blinker = np.array([
-[0,0,0],
-[1,1,1],
-[0,0,0]]
-    )
+    [0, 0, 0],
+    [1, 1, 1],
+    [0, 0, 0]]
+)
 
 glider_gun = np.array([
     [0, 0, 0, 0, 1, 1, 0, 0, 0],
@@ -49,30 +49,35 @@ glider_gun = np.array([
     [0, 0, 1, 1, 0, 0, 0, 0, 0]
 ])
 
+
 class Game:
-    def __init__(game,Size):
-        game.board = np.zeros((Size, Size))
-    
+    def __init__(self, size):
+        self.board = np.zeros((size, size))
+
     def play(self):
         print("Playing life. Press ctrl + c to stop.")
         pyplot.ion()
         while True:
-            self.move(); self.show()
+            self.move()
+            self.show()
             pyplot.pause(0.0000005)
 
     def move(self):
-        STENCIL = np.array([[1, 1, 1], [1, 0, 1], [1, 1, 1]])
-        NeighbourCount = convolve2d(self.board, STENCIL, mode='same')
+        stencil = np.array([[1, 1, 1], [1, 0, 1], [1, 1, 1]])
+        neighbourcount = convolve2d(self.board, stencil, mode='same')
 
         for i in range(self.board.shape[0]):
             for j in range(self.board.shape[1]):
-                self.board[i,j]=1 if (NeighbourCount[i, j]==3 or (NeighbourCount[i, j]==2 and self.board[i,j])) else 0
+                self.board[i, j] = 1 if (
+                            neighbourcount[i, j] == 3
+                            or (neighbourcount[i, j] == 2
+                                and self.board[i, j])
+                                        ) else 0
 
-    def __setitem__(self, key, value): 
+    def __setitem__(self, key, value):
         self.board[key] = value
 
-
     def show(self):
-        pyplot.clf() 
+        pyplot.clf()
         pyplot.matshow(self.board, fignum=0, cmap='binary')
         pyplot.show()
